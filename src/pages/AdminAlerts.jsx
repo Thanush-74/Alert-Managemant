e7e4eb4cfb09a456f29e4079a646aff93a0bc710
Thanushkumar <dhanushkumar1420@gmail.com>
2026-03-30 17:08:39 +0530

    practice 1 change

--- src/pages/AdminAlerts.jsx
+++ src/pages/AdminAlerts.jsx
@@ -64,7 +64,7 @@ function AdminAlerts() {
     startIndex + alertsPerPage,
   );
   const totalPages = Math.ceil(filteredAlerts.length / alertsPerPage);
-console.log("dhanush")
+console.log("practice 1");
   return (
     <>
       <style>{`
